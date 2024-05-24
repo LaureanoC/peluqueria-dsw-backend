ff2e5f3da2aeede4575ef415162084960056e38f
FCVenadoTuerto <123023634+FCVenadoTuerto@users.noreply.github.com>
2024-05-24 16:54:50 -0300

    Add src files

---
+++ src/usuarios/usuario.controler.ts
@@ -0,0 +1,76 @@
+import { Request, Response, NextFunction } from 'express'
+import { UsuarioRepository } from './usuario.repository.js'
+import { Usuario } from './usuario.entity.js'
+
+const repository = new UsuarioRepository()
+
+function sanitizeUsuarioInput(req: Request, res: Response, next: NextFunction) {
+  req.body.sanitizedInput = {
+    dni: req.body.dni,
+    nombre: req.body.nombre,
+    mail: req.body.mail,
+    contraseña: req.body.contraseña,
+    telefono: req.body.telefono,
+    tipo: req.body.tipo,
+  }
+
+  Object.keys(req.body.sanitizedInput).forEach((key) => {
+    if (req.body.sanitizedInput[key] === undefined) {
+      delete req.body.sanitizedInput[key]
+    }
+  })
+  next()
+}
+
+function findAll(req: Request, res: Response) {
+  res.json({ data: repository.findAll() })
+}
+
+function findOne(req: Request, res: Response) {
+  const id = req.params.id
+  const usuario = repository.findOne({ id })
+  if (!usuario) {
+    return res.status(404).send({ message: 'Usuario no encontrado' })
+  }
+  res.json({ data: usuario })
+}
+
+function add(req: Request, res: Response) {
+  const input = req.body.sanitizedInput
+
+  const usuarioInput = new Usuario(
+    input.dni,
+    input.nombre,
+    input.mail,
+    input.contraseña,
+    input.telefono,
+    input.tipo,
+  )
+
+  const usuario = repository.add(usuarioInput)
+  return res.status(201).send({ message: 'Usuario creado', data: usuario })
+}
+
+function update(req: Request, res: Response) {
+  req.body.sanitizedInput.id = req.params.id
+  const usuario = repository.update(req.body.sanitizedInput)
+
+  if (!usuario) {
+    return res.status(404).send({ message: 'Usuario no encontrado' })
+  }
+
+  return res.status(200).send({ message: 'Usuario actualizado correctamente', data: usuario })
+}
+
+function remove(req: Request, res: Response) {
+  const id = req.params.id
+  const usuario = repository.delete({ id })
+
+  if (!usuario) {
+    res.status(404).send({ message: 'Usuario no encontrado' })
+  } else {
+    res.status(200).send({ message: 'Usuario eliminado correctamente' })
+  }
+}
+
+export { sanitizeUsuarioInput, findAll, findOne, add, update, remove }
---
+++ src/usuarios/usuario.repository.ts
@@ -0,0 +1,47 @@
+import { Repository } from '../shared/repository.js'
+import { Usuario } from './usuario.entity.js'
+
+const usuarios = [
+  new Usuario(
+    30199820,
+    'Laureano Fernández',
+    'laureanofernandez98@gmail.com',
+    'CSFTMRJO##',
+    5493415802992,
+    'cliente',
+  ),
+]
+
+export class UsuarioRepository implements Repository<Usuario> {
+  public findAll(): Usuario[] | undefined {
+    return usuarios
+  }
+
+  public findOne(item: { id: string }): Usuario | undefined {
+    return usuarios.find((usuario) => usuario.id === item.id)
+  }
+
+  public add(item: Usuario): Usuario | undefined {
+    usuarios.push(item)
+    return item
+  }
+
+  public update(item: Usuario): Usuario | undefined {
+    const usuarioIndex = usuarios.findIndex((usuario) => usuario.id === item.id)
+
+    if (usuarioIndex !== -1) {
+      usuarios[usuarioIndex] = { ...usuarios[usuarioIndex], ...item }
+    }
+    return usuarios[usuarioIndex]
+  }
+
+  public delete(item: { id: string }): Usuario | undefined {
+    const usuarioIndex = usuarios.findIndex((usuario) => usuario.id === item.id)
+
+    if (usuarioIndex !== -1) {
+      const deletedUsuarios = usuarios[usuarioIndex]
+      usuarios.splice(usuarioIndex, 1)
+      return deletedUsuarios
+    }
+  }
+}
